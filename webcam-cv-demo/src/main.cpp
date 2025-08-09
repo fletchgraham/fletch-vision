af2bd57a17a9f788a38415a8f61f10cb3c35b770
fletchgraham <sfletchgraham@gmail.com>
2025-08-09 09:16:47 -0600

    a test change

--- webcam-cv-demo/src/main.cpp
+++ webcam-cv-demo/src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     std::cout << "=== Webcam CV Demo ===" << std::endl;
     std::cout << "Basic C++ compilation successful!" << std::endl;
     std::cout << "This confirms our build environment works." << std::endl;
+    std::cout << "And this was typed by fletch himself" << std::endl;
     std::cout << std::endl;
     
     // Simple countdown demo
